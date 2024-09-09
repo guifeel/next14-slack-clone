edd728e5a7dfcfc7942fab21ee5f8b97f8b50539
guifeel <guifeel@qq.com>
2024-09-09 18:56:02 +0800

    UserButton

---
+++ src/app/features/auth/components/UserButton.tsx
@@ -0,0 +1,47 @@
+import { useAuthActions } from "@convex-dev/auth/react";
+import { Loader, LogOut } from "lucide-react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "../../../../components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../../../components/ui/dropdown-menu";
+import { useCurrentUser } from "../hooks/useCurrentUser";
+
+const UserButton = () => {
+  const { data, isLoading } = useCurrentUser();
+  const { signOut } = useAuthActions();
+
+  if (isLoading)
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+
+  if (!data) return null;
+
+  const { name, image, email } = data;
+
+  const callbackName = name?.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage src={image} alt={name} />
+          <AvatarFallback>{callbackName}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem onClick={() => signOut()}>
+          <LogOut className="size-4 mr-2" />
+          登出
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
+import UserButton from "./features/auth/components/UserButton";
 
 export default function Home() {
   const { signOut } = useAuthActions();
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       已登录
+      <UserButton />
       <Button onClick={() => signOut()}>退出</Button>
     </main>
   );
