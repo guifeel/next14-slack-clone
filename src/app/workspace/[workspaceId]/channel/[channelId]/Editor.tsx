cd870807102501c00d220efbbda68fabf3e706e9
guifeel <guifeel@qq.com>
2024-09-25 15:05:50 +0800

    构建Quill

--- src/app/workspace/[workspaceId]/channel/[channelId]/Editor.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/Editor.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useRef } from "react";
+
+import Quill, { type QuillOptions } from "quill";
+import "quill/dist/quill.snow.css";
+
 const Editor = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const container = containerRef.current;
+    const editorContainer = container.appendChild(
+      container.ownerDocument.createElement("div")
+    );
+
+    const options: QuillOptions = {
+      theme: "snow",
+    };
+
+    new Quill(editorContainer, options);
+    return () => {
+      if (container) {
+        container.innerHTML = "";
+      }
+    };
+  }, []);
   return (
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-300 rounded-md overflow-hidden bg-white focus-within:border-slate-300 focus-within:shadow-sm transition">
-        Editor
+        <div ref={containerRef} />
       </div>
     </div>
   );
