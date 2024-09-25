156f443d91c538ef8d945090c32cb205d9f83742
guifeel <guifeel@qq.com>
2024-09-25 18:32:14 +0800

    Build update variant

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,8 +7,13 @@ import "quill/dist/quill.snow.css";
 import { ImageIcon, Smile } from "lucide-react";
 import { MdSend } from "react-icons/md";
 import { PiTextAa } from "react-icons/pi";
+import Hint from "./Hint";
 
-const Editor = () => {
+interface EditorProps {
+  variant?: "create" | "update";
+}
+
+const Editor = ({ variant = "create" }: EditorProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -35,34 +40,75 @@ const Editor = () => {
       <div className="flex flex-col border border-slate-300 rounded-md overflow-hidden bg-white focus-within:border-slate-300 focus-within:shadow-sm transition">
         <div ref={containerRef} className="h-full ql-custom" />
         <div className="flex px-2 pb-2 z-[5]">
-          <Button
-            disabled={false}
-            size="iconSm"
-            variant="ghost"
-            onClick={() => {}}
-          >
-            <PiTextAa className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            size="iconSm"
-            variant="ghost"
-            onClick={() => {}}
-          >
-            <Smile className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            size="iconSm"
-            variant="ghost"
-            onClick={() => {}}
-          >
-            <ImageIcon className="size-4" />
-          </Button>
-          <Button>
-            <MdSend className="size-4" />
-          </Button>
+          <Hint label="隐藏/显示">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <PiTextAa className="size-4" />
+            </Button>
+          </Hint>
+          <Hint label="表情">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <Smile className="size-4" />
+            </Button>
+          </Hint>
+          {variant === "create" && (
+            <Hint label="图片">
+              <Button
+                disabled={false}
+                size="iconSm"
+                variant="ghost"
+                onClick={() => {}}
+              >
+                <ImageIcon className="size-4" />
+              </Button>
+            </Hint>
+          )}
+
+          {variant === "create" && (
+            <Button
+              disabled={false}
+              size="iconSm"
+              onClick={() => {}}
+              className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+            >
+              <MdSend className="size-4" />
+            </Button>
+          )}
+          {variant === "update" && (
+            <div className="ml-auto flex items-center justify-center gap-x-2">
+              <Button
+                variant="outline"
+                disabled={false}
+                size="sm"
+                onClick={() => {}}
+              >
+                取消
+              </Button>
+              <Button
+                disabled={false}
+                size="sm"
+                onClick={() => {}}
+                className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+              >
+                保存
+              </Button>
+            </div>
+          )}
         </div>
+      </div>
+      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+        <p>
+          <strong>Shift + Enter</strong> 进行换行操作
+        </p>
       </div>
     </div>
   );
