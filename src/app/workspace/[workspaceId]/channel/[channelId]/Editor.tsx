2261005d9c8d17bc5049d8a3ff0b6aed349b524d
guifeel <guifeel@qq.com>
2024-09-25 17:22:32 +0800

    Quill图标显示

--- src/app/workspace/[workspaceId]/channel/[channelId]/Editor.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/Editor.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useRef } from "react";
 
+import { Button } from "@/components/ui/button";
 import Quill, { type QuillOptions } from "quill";
 import "quill/dist/quill.snow.css";
+
+import { ImageIcon, Smile } from "lucide-react";
+import { MdSend } from "react-icons/md";
+import { PiTextAa } from "react-icons/pi";
 
 const Editor = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -29,6 +34,35 @@ const Editor = () => {
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-300 rounded-md overflow-hidden bg-white focus-within:border-slate-300 focus-within:shadow-sm transition">
         <div ref={containerRef} className="h-full ql-custom" />
+        <div className="flex px-2 pb-2 z-[5]">
+          <Button
+            disabled={false}
+            size="iconSm"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            <PiTextAa className="size-4" />
+          </Button>
+          <Button
+            disabled={false}
+            size="iconSm"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            <Smile className="size-4" />
+          </Button>
+          <Button
+            disabled={false}
+            size="iconSm"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            <ImageIcon className="size-4" />
+          </Button>
+          <Button>
+            <MdSend className="size-4" />
+          </Button>
+        </div>
       </div>
     </div>
   );
