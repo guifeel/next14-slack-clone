4ed5cd535cb8439b442c4d0e39dc35f4467ffa06
guifeel <guifeel@qq.com>
2024-09-28 17:02:15 +0800

    Schema finalization

--- convex/messages.ts
+++ convex/messages.ts
@@ -22,7 +22,7 @@ export const create = mutation({
     image: v.optional(v.id("_storage")),
     workspaceId: v.id("workspaces"),
     channelId: v.optional(v.id("channels")),
-    // conversationId: v.optional(v.id("conversations")),
+    conversationId: v.optional(v.id("conversations")),
     parentMessageId: v.optional(v.id("messages")),
   },
   handler: async (ctx, args) => {
@@ -38,11 +38,24 @@ export const create = mutation({
       throw new Error("未鉴权");
     }
 
+    let _conversationId = args.conversationId;
+
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+
+      if (!parentMessage) {
+        throw new Error("父级消息未找到");
+      }
+
+      _conversationId = parentMessage.conversationId;
+    }
+
     const messageId = await ctx.db.insert("messages", {
       memberId: member._id,
       body: args.body,
       image: args.image,
       channelId: args.channelId,
+      conversationId: _conversationId,
       workspaceId: args.workspaceId,
       parentMessageId: args.parentMessageId,
     });
