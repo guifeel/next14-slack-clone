e9c193009e1b1469519163c91721dc2a5e4273ff
guifeel <guifeel@qq.com>
2024-09-29 22:33:13 +0800

    Hint套Time

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,9 +1,13 @@
-import { format } from "date-fns";
+import { format, isToday, isYesterday } from "date-fns";
 import dynamic from "next/dynamic";
 import { Doc, Id } from "../../convex/_generated/dataModel";
+import Hint from "./Hint";
 
 const Renderer = dynamic(() => import("@/components/Renderer"), { ssr: false });
 
+const formatFullTime = (date: Date) => {
+  return `${isToday(date) ? "今天" : isYesterday(date) ? "昨天" : format(date, "MMM d,yyyy")} 于 ${format(date, "h:mm:ss a")}`;
+};
 interface MessageProps {
   id: Id<"messages">;
   memberId: Id<"members">;
@@ -54,9 +58,11 @@ const Message = ({
   return (
     <div className="flex flex-col hover:bg-gray-100/60 gap-2 px-5 py-1.5 group relative:">
       <div className="flex items-start gap-2">
-        <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100">
-          {format(new Date(createdAt), "hh:mm")}
-        </button>
+        <Hint label={formatFullTime(new Date(createdAt))}>
+          <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100">
+            {format(new Date(createdAt), "hh:mm")}
+          </button>
+        </Hint>
       </div>
       <Renderer value={body} />
     </div>
