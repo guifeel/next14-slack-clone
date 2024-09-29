6253add2b57dd9f0acd137e4c5a331ba71cacfa8
guifeel <guifeel@qq.com>
2024-09-29 16:54:09 +0800

    MessageList消息时间渲染

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,4 +1,5 @@
 import { GetMessageReturnType } from "@/app/features/messages/api/useGetMessage";
+import { format, isToday, isYesterday } from "date-fns";
 
 interface MessageListProps {
   memberName?: string;
@@ -12,6 +13,13 @@ interface MessageListProps {
   canLoadMore: boolean;
 }
 
+const formatDateLabel = (dateStr: string) => {
+  const date = new Date(dateStr);
+  if (isToday(date)) return "今天";
+  if (isYesterday(date)) return "昨天";
+  return format(date, "EEEE,MMMM d");
+};
+
 const MessageList = ({
   memberImage,
   memberName,
@@ -23,10 +31,33 @@ const MessageList = ({
   isLoadingMore,
   canLoadMore,
 }: MessageListProps) => {
+  const groupedMessages = data?.reduce(
+    (groups, message) => {
+      const date = new Date(message._creationTime);
+      const dateKey = format(date, "yyyy-MM-dd");
+      if (!groups[dateKey]) {
+        groups[dateKey] = [];
+      }
+      groups[dateKey].unshift(message);
+      return groups;
+    },
+    {} as Record<string, typeof data>
+  );
+
   return (
     <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
-      {data?.map((message) => (
-        <div key={message._id}>{JSON.stringify(message)}</div>
+      {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
+        <div key={dateKey}>
+          <div className="text-center my-2 relative">
+            <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+            <span className="relative inline-block bg-white px-4 py-1 rounded-md text-xs border border-gray-300 shadow-sm">
+              {formatDateLabel(dateKey)}
+            </span>
+          </div>
+          {messages.map((message, index) => {
+            return <div key={index}>{JSON.stringify(message)}</div>;
+          })}
+        </div>
       ))}
     </div>
   );
