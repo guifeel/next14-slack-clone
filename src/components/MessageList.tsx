5fca18abc34422ae734004648c4eb21a4615c460
guifeel <guifeel@qq.com>
2024-10-02 22:23:36 +0800

    修复之前的。以后是开始Thread的复制

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -102,40 +102,8 @@ const MessageList = ({
               />
             );
           })}
-          <div
-            className="h-1"
-            ref={(el) => {
-              if (el) {
-                const observer = new IntersectionObserver(
-                  ([entry]) => {
-                    if (entry.isIntersecting && canLoadMore) {
-                      loadMore();
-                    }
-                  },
-                  { threshold: 1.0 }
-                );
-
-                observer.observe(el);
-                return () => observer.disconnect();
-              }
-            }}
-          />
-          {isLoadingMore && (
-            <div className="text-center my-2 relative">
-              <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
-              <span className="relative inline-block bg-white px-4 py-1 rounded-md text-xs border border-gray-300 shadow-sm">
-                <Loader className="size-4 animate-spin" />
-              </span>
-            </div>
-          )}
-          {variant === "channel" && channelName && channelCreationTime && (
-            <ChannelHero
-              name={channelName}
-              creationTime={channelCreationTime}
-            />
-          )}
         </div>
-      ))}{" "}
+      ))}
       <div
         className="h-1"
         ref={(el) => {
